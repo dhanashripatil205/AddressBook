84e959f2fa3f442479b07fb9a02b7f2e54835f92
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-04 02:44:56 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 ﻿namespace AddressBookProgram
 {
-    public class Program
+    public class AddressBook1
     {
         public static void Main(string[] args)
         {
-            // Console.WriteLine("******** Welcome to Address Book Program ********");
-
             AddressBook obj = new AddressBook();
             for (int i = 1; i <= 2; i++)
             {
@@ -13,8 +11,15 @@
                 obj.CreateUser();
             }
             obj.ShowAllPersonsInList();
+            Console.WriteLine("\n Do you want to Edit Information: if yes=1, no=2");
+            int A = Convert.ToInt32(Console.ReadLine());
+            if (A == 1)
+            {
+                obj.EditUserInformation();
+                Console.WriteLine("\n Contact Edited");
+                obj.ShowAllPersonsInList();
+            }
+
         }
-
-
     }
 }
