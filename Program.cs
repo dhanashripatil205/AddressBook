3f486afd3a7c0acd7d2ba5562de342b6d43fd647
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-04 03:17:25 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -4,22 +4,53 @@
     {
         public static void Main(string[] args)
         {
-            AddressBook obj = new AddressBook();
+            bool choice = true;
+            AddressBook ab = new AddressBook();
             for (int i = 1; i <= 2; i++)
             {
                 Console.WriteLine("\n Enter Contact Details {0}", i);
-                obj.CreateUser();
+                ab.CreateUser();
             }
-            obj.ShowAllPersonsInList();
-            Console.WriteLine("\n Do you want to Edit Information: if yes=1, no=2");
-            int A = Convert.ToInt32(Console.ReadLine());
-            if (A == 1)
+            ab.ShowAllPersonsInList();
+            static void Userchoice()
             {
-                obj.EditUserInformation();
-                Console.WriteLine("\n Contact Edited");
-                obj.ShowAllPersonsInList();
+                Console.WriteLine("Choose one of the following options: ");
+                Console.WriteLine("#1 Create new user");
+                Console.WriteLine("#2 Edit user information");
+                Console.WriteLine("#3 Delete existing user");
+                Console.WriteLine("#4 Show all users in adressBook");
+                Console.WriteLine("#5 Exit");
             }
+            while (choice)
+            {
+                Userchoice();
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        ab.CreateUser();
+                        Console.WriteLine("\n ***Contact Added***");
+                        ab.ShowAllPersonsInList();
+                        break;
+                    case 2:
+                        ab.EditUserInformation();
+                        Console.WriteLine("\n ***Contact Edited***");
+                        ab.ShowAllPersonsInList();
+                        break;
+                    case 3:
+                        ab.DeleteContact();
+                        Console.WriteLine("\n ***Contact Deleted***");
+                        ab.ShowAllPersonsInList();
+                        break;
+                    case 4:
+                        ab.ShowAllPersonsInList();
+                        break;
+                    case 5:
+                        choice = false;
+                        break;
+                }
 
+            }
         }
     }
 }
