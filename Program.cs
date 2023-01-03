b4ed9ed813167579336ac0af7b73a43284efeb73
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-04 01:59:54 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@
             // Console.WriteLine("******** Welcome to Address Book Program ********");
 
             AddressBook obj = new AddressBook();
-            obj.CreateUser();
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("\n Enter Contact Details {0}", i);
+                obj.CreateUser();
+            }
             obj.ShowAllPersonsInList();
         }
 
