c9d68f1e3fc1aa3dcaaa213efb6660f9310d94f8
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-04 22:22:25 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -1,56 +1,31 @@
-﻿namespace AddressBookProgram
-{
-    public class AddressBook1
-    {
-        public static void Main(string[] args)
-        {
-            bool choice = true;
-            AddressBook ab = new AddressBook();
-            for (int i = 1; i <= 2; i++)
-            {
-                Console.WriteLine("\n Enter Contact Details {0}", i);
-                ab.CreateUser();
-            }
-            ab.ShowAllPersonsInList();
-            static void Userchoice()
-            {
-                Console.WriteLine("Choose one of the following options: ");
-                Console.WriteLine("#1 Create new user");
-                Console.WriteLine("#2 Edit user information");
-                Console.WriteLine("#3 Delete existing user");
-                Console.WriteLine("#4 Show all users in adressBook");
-                Console.WriteLine("#5 Exit");
-            }
-            while (choice)
-            {
-                Userchoice();
-                int Choice = Convert.ToInt32(Console.ReadLine());
-                switch (Choice)
-                {
-                    case 1:
-                        ab.CreateUser();
-                        Console.WriteLine("\n ***Contact Added***");
-                        ab.ShowAllPersonsInList();
-                        break;
-                    case 2:
-                        ab.EditUserInformation();
-                        Console.WriteLine("\n ***Contact Edited***");
-                        ab.ShowAllPersonsInList();
-                        break;
-                    case 3:
-                        ab.DeleteContact();
-                        Console.WriteLine("\n ***Contact Deleted***");
-                        ab.ShowAllPersonsInList();
-                        break;
-                    case 4:
-                        ab.ShowAllPersonsInList();
-                        break;
-                    case 5:
-                        choice = false;
-                        break;
-                }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-5 Program to Add multiple person in a Address Book
+
+            Console.WriteLine("Welcome to Address Book");
+
+            int choice = 0;
+            while (choice != 2)
+            {
+                Console.WriteLine("1.Perform Operations \n2.Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice != 2)
+                    AddressBook1.AddPerson();
             }
+            AddressBook1.ListPeople();
+            Console.WriteLine("Program Ends: Address Book: Press Enter");
+            Console.ReadLine();
+
         }
     }
 }
